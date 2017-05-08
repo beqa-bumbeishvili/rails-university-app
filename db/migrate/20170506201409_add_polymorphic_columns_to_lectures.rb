d00b6855fbb95eb70b1863c6dfb96c740f5c8f02
beqa-bumbeishvili <bumbeishvili.beqa@gmail.com>
2017-05-08 17:03:05 +0400

    update migration

--- db/migrate/20170506201409_add_polymorphic_columns_to_lectures.rb
+++ db/migrate/20170506201409_add_polymorphic_columns_to_lectures.rb
@@ -1,7 +1,7 @@
 class AddPolymorphicColumnsToLectures < ActiveRecord::Migration[5.0]
   def change
-    add_column :lectures, :mentor_id, :integer
-    add_column :lectures, :mentor_type, :string
-    add_index :lectures, [:mentor_id, :mentor_type]
+    add_column :lectures, :owner_object_id, :integer
+    add_column :lectures, :owner_object_type, :string
+    add_index :lectures, [:owner_object_id, :owner_object_type]
   end
 end
